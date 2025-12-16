ffd01d6ac67d71e41854de98582395ce7412d4d3
Samuel Lortie <samuellortie2001@gmail.com>
2025-12-16 13:39:45 -0500

    implementation de la fonction lire_tickets

--- gestion.py
+++ gestion.py
@@ -12,3 +12,16 @@ def creer_ticket(tickets):
     tickets.append(nouveau_ticket)
 
     print(nouveau_ticket)
+
+#Read
+def lire_tickets(tickets):
+    if not tickets:
+        print("Aucun ticket.")
+        return
+    print("====LISTE DE TICKETS====")
+    for i, ticket in enumerate(tickets, 1):
+        print(f"\nTicket # {i}")
+        print(f'Classe : {ticket["classe"]}')
+        print(f'Titre : {ticket["titre"]}')
+        print(f'Description :\n{ticket["description"]}')
+        print(f'État : {ticket["état"]}')
