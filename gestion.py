99c2b01c8012881386e634597e040fca6774ea8e
Samuel Lortie <samuellortie2001@gmail.com>
2025-12-17 12:43:49 -0500

    ajout de feature update

--- gestion.py
+++ gestion.py
@@ -25,3 +25,23 @@ def lire_tickets(tickets):
         print(f'Titre : {ticket["titre"]}')
         print(f'Description :\n{ticket["description"]}')
         print(f'État : {ticket["état"]}')
+
+#Update
+def update_ticket(tickets):
+    lire_tickets(tickets)
+    if not tickets:
+        return
+    index = int(input("Entrez le numero du ticket a modifier : ")) - 1
+    ticket = tickets[index]
+    print("Laisser vide pour ne pas modifier")
+    titre = input(f'titre actuel : {ticket["titre"]}\nNouveau titre: ')
+    if titre:
+        ticket["titre"] = titre
+    description = input(f'Descrition actuel:\n{ticket["description"]}\nNouvelle description : ')
+    if description:
+        ticket["description"] = description
+    etat = input(f'etat actuel : {ticket["état"]}\n(Ouvert / En Cours / Fermé): ')
+    if etat:
+        ticket["état"] = etat
+    ticket["classe"] = classe_ticket(ticket["titre"] + " " + ticket["description"])
+    print("\nTicket Modifié")
